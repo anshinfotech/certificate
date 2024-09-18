a4437f41ded8a67327005b7e37e0fbab8a4ba482
paramlotey <plotey88@gmail.com>
2024-09-18 11:16:54 +0530

    final

--- index.js
+++ index.js
@@ -15,20 +15,81 @@ server.use(
     origin: "*",
   })
 );
+const DB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://anshinfotech1:f8QiKYpsDE53Cs6l@cluster0.iqayfbm.mongodb.net/certificate",
+      {
+        serverSelectionTimeoutMS: 30000, // 30 seconds
+        socketTimeoutMS: 45000, // 45 seconds
+      }
+    );
+    console.log("databse connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+DB();
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  college: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model("Users", userSchema);
+
 server.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));  // Corrected path
+  res.sendFile(path.join(__dirname, "index.html")); // Corrected path
 });
 
 server.post("/get-certificate", async (req, res) => {
-  const { name, fathername } = req.body;
+  const { name, fatherName, email, college, mobile } = req.body;
   try {
-    const doc = new PDFDocument();
+    const existingUser = await userModel.findOne({ email, mobile });
+    if (existingUser) {
+      return res.status(200).sendFile(path.join(__dirname, "./Error.html"));
+    }
+
+    const newUser = await userModel.create({
+      name,
+      fatherName,
+      email,
+      mobile,
+      college,
+    });
+
+    // Set headers for PDF download and prevent caching
     res.setHeader(
       "Content-disposition",
       "attachment; filename=certificate.pdf"
     );
     res.setHeader("Content-type", "application/pdf");
+    res.setHeader(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
 
+    const doc = new PDFDocument();
     doc.pipe(res);
 
     // Add certificate background
@@ -42,6 +103,8 @@ server.post("/get-certificate", async (req, res) => {
 
     // Insert additional text
     doc.fontSize(15).text("Congratulations on your achievement!", 200, 400);
+
+    // Finalize the PDF and end the stream
     doc.end();
   } catch (error) {
     console.log(error);
