22d910282c44360a073c89ebd02660a4b10d1b03
Jaspreet Singh <js9316713287@gmail.com>
2024-10-08 10:44:03 +0530

    GGSU Certificated added

--- index.js
+++ index.js
@@ -81,59 +81,9 @@ server.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "Error.html")); // Error path
 });
 
-// server.post("/get-certificate", async (req, res) => {
-//   const { name, fatherName, email, college, mobile, sem, stream, course } =
-//     req.body;
-//   try {
-//     const existingUser = await userModel.findOne({ email, mobile });
-//     console.log(existingUser);
-//     if (!existingUser) {
-//       const newUser = await userModel.create({
-//         name,
-//         fatherName,
-//         email,
-//         mobile,
-//         college,
-//         sem,
-//         stream,
-//         course,
-//       });
-
-//       // Set headers for PDF download and prevent caching
-//       res.setHeader(
-//         "Content-disposition",
-//         "attachment; filename=certificate.pdf"
-//       );
-//       res.setHeader("Content-type", "application/pdf");
-//       res.setHeader(
-//         "Cache-Control",
-//         "no-store, no-cache, must-revalidate, proxy-revalidate"
-//       );
-//       res.setHeader("Pragma", "no-cache");
-//       res.setHeader("Expires", "0");
-
-//       const doc = new PDFDocument({
-//         size: [2000, 1414], // Custom page size matching the image
-//       });
-//       doc.pipe(res);
-
-//       doc.image(path.join(__dirname, "Riet.jpg"), 0, 0, {
-//         width: 2000,
-//         height: 1414,
-//       });
-//       doc.fontSize(100).text(name, 0, 720, { align: "center" });
-
-//       // Finalize the PDF and end the stream
-//       doc.end();
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Error generating certificate");
-//   }
-// });
-
 server.post("/get-certificate", async (req, res) => {
-  const { name, fatherName, email, college, mobile, sem, stream, course } = req.body;
+  const { name, fatherName, email, college, mobile, sem, stream, course } =
+    req.body;
   try {
     let user = await userModel.findOne({ email, mobile });
 
@@ -151,9 +101,15 @@ server.post("/get-certificate", async (req, res) => {
     }
 
     // Set headers for PDF download and prevent caching
-    res.setHeader("Content-disposition", "attachment; filename=certificate.pdf");
+    res.setHeader(
+      "Content-disposition",
+      "attachment; filename=certificate.pdf"
+    );
     res.setHeader("Content-type", "application/pdf");
-    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    res.setHeader(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
     res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", "0");
 
@@ -165,50 +121,58 @@ server.post("/get-certificate", async (req, res) => {
     // Generate the certificate PDF based on the college
     // Use the appropriate college template
 
-    
-      // 1)
-      // *********   Lyallpur Khalsa College  ***********
-      // doc.image(path.join(__dirname, "cert.jpg"), 0, 0, {
-      //   width: 2000,
-      //   height: 1414,
-      // });
-      // doc.fontSize(100).text(name, 0, 610 , {align : "center"});
+    // 1)
+    // *********   Lyallpur Khalsa College  ***********
+    // doc.image(path.join(__dirname, "cert.jpg"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(100).text(name, 0, 610 , {align : "center"});
 
-      // 2)
-      //********   Grur Nanak Dev Engineering College    ************
-      // doc.image(path.join(__dirname, "GNE.jpg"), 0, 0, {
-      //   width: 2000,
-      //   height: 1414,
-      // });
-      // doc.fontSize(100).text(name, 0, 720, { align: "center" });
-      
-      
-      // 3)
-      //********   Ramgarhia Institue of engeniring and technology (Riet)    ************
-      
-      // doc.image(path.join(__dirname, "Riet.jpg"), 0, 0, {
-        //   width: 2000,
-        //   height: 1414,
-        // });
-        // doc.fontSize(100).text(name, 0, 720, { align: "center" });
-        
-        // 4)
-        //********   Gulzar Group Of Institutions    ************
-        
-        doc.image(path.join(__dirname, "GGI.jpg"), 0, 0, {
-          width: 2000,
-          height: 1414,
-        });
-        doc.fontSize(80).text(name, 0, 720, { align: "center" });
+    // 2)
+    //********   Grur Nanak Dev Engineering College    ************
+    // doc.image(path.join(__dirname, "GNE.jpg"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(100).text(name, 0, 720, { align: "center" });
+
+    // 3)
+    //********   Ramgarhia Institue of engeniring and technology (Riet)    ************
+
+    // doc.image(path.join(__dirname, "Riet.jpg"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(100).text(name, 0, 720, { align: "center" });
+
+    // 4)
+    //********   Gulzar Group Of Institutions    ************
+
+    // doc.image(path.join(__dirname, "GGI.jpg"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(75).text(name, 0, 720, { align: "center" });
+
+    // 5)
+    //********   Gulzar Group Of Institutions    ************
+
+    doc.image(path.join(__dirname, "GGSU.jpg"), 0, 0, {
+      width: 2000,
+      height: 1414,
+    });
+    doc.fontSize(75).text(name, 0, 720, { align: "center" });
 
     // Finalize the PDF and end the stream
     doc.end();
   } catch (error) {
     console.log(error);
-    res.status(500).send({ success: false, message: "Error generating certificate" });
+    res
+      .status(500)
+      .send({ success: false, message: "Error generating certificate" });
   }
 });
-
 
 server.listen(8000, () => {
   console.log("server running");
