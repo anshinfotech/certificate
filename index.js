e1dc90b079b12a877f2334669eac50ad00c8aeca
Jaspreet Singh <js9316713287@gmail.com>
2024-10-16 10:09:36 +0530

    BFGI certificate added

--- index.js
+++ index.js
@@ -181,7 +181,14 @@ server.post("/get-certificate", async (req, res) => {
 
 
      // ********  RIMT College Mandi Gobindgarh    ************
-    doc.image(path.join(__dirname, "RIMT.png"), 0, 0, {
+    // doc.image(path.join(__dirname, "RIMT.png"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(75).text(name, 0, 720, { align: "center" });
+
+     // ********  RIMT College Mandi Gobindgarh    ************
+    doc.image(path.join(__dirname, "BFGI.png"), 0, 0, {
       width: 2000,
       height: 1414,
     });
