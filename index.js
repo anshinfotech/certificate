1d519a6e1e7cf5418c834590206b3a95ee0c9364
Hukam <singhjaspreet2425@gmail.com>
2024-11-11 06:52:29 +0530

    SCD certificate added

--- index.js
+++ index.js
@@ -209,7 +209,14 @@ server.post("/get-certificate", async (req, res) => {
     // doc.fontSize(75).text(name, 0, 720, { align: "center" });
 
     // ********  GNIMT College    ************
-    doc.image(path.join(__dirname, "gnimt.png"), 0, 0, {
+    // doc.image(path.join(__dirname, "gnimt.png"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(75).text(name, 0, 720, { align: "center" });
+
+     // ********  GNIMT College    ************
+    doc.image(path.join(__dirname, "SCD.jpg"), 0, 0, {
       width: 2000,
       height: 1414,
     });
