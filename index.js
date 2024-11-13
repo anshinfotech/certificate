7379bdca0a84a4753621c76757dd2207396ba4c2
Hukam <singhjaspreet2425@gmail.com>
2024-11-13 10:31:13 +0530

    Certificate added

--- index.js
+++ index.js
@@ -215,8 +215,15 @@ server.post("/get-certificate", async (req, res) => {
     // });
     // doc.fontSize(75).text(name, 0, 720, { align: "center" });
 
-     // ********  GNIMT College    ************
-    doc.image(path.join(__dirname, "SCD.jpg"), 0, 0, {
+     // ********  SCD Boys College    ************
+    // doc.image(path.join(__dirname, "SCD.jpg"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(75).text(name, 0, 720, { align: "center" });
+
+    // ********  SCD Boys College    ************
+    doc.image(path.join(__dirname, "Modern.png"), 0, 0, {
       width: 2000,
       height: 1414,
     });
