cc4856cef6b27f4a885e7320c3c2184c562f09bb
Ansh Infotech <52543676+anshinfotech@users.noreply.github.com>
2025-02-15 18:22:04 +0530

    Update index.js SBBSU added
    Hello World

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ server.post("/get-certificate", async (req, res) => {
     // doc.fontSize(75).text(name, 0, 720, {align:"center"});
 
     // ********  SCD Boys College    ************
-    doc.image(path.join(__dirname, "LGC.png"), 0, 0, {
+    doc.image(path.join(__dirname, "SBBSU.jpg"), 0, 0, {
       width: 2000,
       height: 1414,
     });
