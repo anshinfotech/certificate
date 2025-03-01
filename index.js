899b67b84c6eb36503772979af44d33b1d9e94a1
Ansh Infotech <52543676+anshinfotech@users.noreply.github.com>
2025-03-01 10:09:32 +0530

    Update index.js Arya College

--- index.js
+++ index.js
@@ -244,7 +244,14 @@ server.post("/get-certificate", async (req, res) => {
     // doc.fontSize(75).text(name, 0, 720, { align: "center" });
 
      //DAVIET AI/ML
-     doc.image(path.join(__dirname, "DAV_Uni.png"), 0, 0, {
+    //  doc.image(path.join(__dirname, "DAV_Uni.png"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(75).text(name, 0, 720, { align: "center" });
+
+    //Arya College DM Workshop
+     doc.image(path.join(__dirname, "AryaCollege.png"), 0, 0, {
       width: 2000,
       height: 1414,
     });
