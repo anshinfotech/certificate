042646ba5bbcb4f38f474bff4c4d4df8b1f31802
Ansh Infotech <52543676+anshinfotech@users.noreply.github.com>
2025-03-19 12:38:20 +0530

    Update index.js LKC College

--- index.js
+++ index.js
@@ -286,12 +286,18 @@ server.post("/get-certificate", async (req, res) => {
     // doc.fontSize(75).text(name, 0, 720, { align: "center" });
 
     //LGC Machine Learning
-     doc.image(path.join(__dirname, "LGC_Machine.png"), 0, 0, {
+    //  doc.image(path.join(__dirname, "LGC_Machine.png"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(75).text(name, 0, 720, { align: "center" });
+
+    //LGC Machine Learning
+     doc.image(path.join(__dirname, "LKC.png"), 0, 0, {
       width: 2000,
       height: 1414,
     });
     doc.fontSize(75).text(name, 0, 720, { align: "center" });
- 
 
     // Finalize the PDF and end the stream
     doc.end();
