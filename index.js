b47e253f6d47494bd3c1a34912cfc9b2f65918e5
Ansh Infotech <52543676+anshinfotech@users.noreply.github.com>
2025-03-26 10:26:14 +0530

    Update index.js PIMT certificate updated

--- index.js
+++ index.js
@@ -322,7 +322,14 @@ server.post("/get-certificate", async (req, res) => {
 
 
     // GGNIMT Certficate
-     doc.image(path.join(__dirname, "GGNIMT.png"), 0, 0, {
+    //  doc.image(path.join(__dirname, "GGNIMT.png"), 0, 0, {
+    //   width: 2000,
+    //   height: 1414,
+    // });
+    // doc.fontSize(75).text(name, 0, 720, { align: "center" });
+
+    // PIMT Certficate
+     doc.image(path.join(__dirname, "PIMT_Shell.png"), 0, 0, {
       width: 2000,
       height: 1414,
     });
