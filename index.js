8aa159e9e1af18628ad6c8a1c06625bae8001a27
Ansh Infotech <52543676+anshinfotech@users.noreply.github.com>
2025-04-15 10:08:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ server.post("/get-certificate", async (req, res) => {
     // });
     // doc.fontSize(75).text(name, 0, 720, { align: "center" });
 
-      doc.image(path.join(__dirname, "RIMT_CERT.jpg"), 0, 0, {
+      doc.image(path.join(__dirname, "SRS_AI.png"), 0, 0, {
       width: 2000,
       height: 1414,
     });
